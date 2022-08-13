652f86d252eeb173a13d577bdc901b613c537ede
jennywoon <cjaeks0394@naver.com>
2022-08-14 00:49:33 +0900

    뷰 라우터 연결, css 수정

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { __getMovies } from "../redux/modules/moviesSlice";
+import logo from "../assets/logo.jpg"
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -27,7 +28,7 @@ const Home = () => {
 export default Home;
 
 const HomeWrap = styled.div`
-  margin: 0 auto;
+  /* margin: 0 auto;
   max-width: 100%;
   height: 92%;
   padding: 10px 0;
@@ -36,34 +37,49 @@ const HomeWrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-start;
+  flex-direction: column; */
+  /* border: 1px solid white; */
+  width: 90%;
+  height: 90%;
+  display: flex;
+  margin: auto;
   flex-direction: column;
+  padding: 20px;
+  align-items: center;
 `;
 
+
 const HomeUpWrap = styled.div`
+  /* border: 1px solid white; */
   box-sizing: border-box;
-  padding: 30px 40px;
-  width: 100%;
+  padding: 30px;
+  /* width: 100%; */
+  width: 80%;
   display: flex;
   align-items: center;
-  justify-content: stretch;
+  /* justify-content: stretch; */
+  justify-content: center;
   flex-direction: row;
-  gap: 40px;
+  gap: 50px;
 `;
 
 const HomeImg = styled.div`
   background-image: url("https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/6GlL/image/RABTPwU4lfTwFZPRD4aeox7Jpz0.jpg");
   background-position: center;
   background-size: cover;
-  width: 100%;
+  width: 450px;
   height: 320px;
   margin: 10px;
   border: 1px solid black;
 `;
 
 const HomeBottomWrap = styled.div`
+  /* border: 1px solid white; */
+  margin-top: 40px;
   box-sizing: border-box;
-  padding: 10px 40px;
+  padding: 10px 50px;
   width: 100%;
+  height: 300px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -72,25 +88,30 @@ const HomeBottomWrap = styled.div`
 `;
 
 const BottomLeftImg = styled.div`
-  background-image: url("https://cdn.catholicnews.co.kr/news/photo/202001/22135_43951_5525.jpg");
+  /* background-image: url("https://cdn.catholicnews.co.kr/news/photo/202001/22135_43951_5525.jpg"); */
+  background-image: url(${logo});
   background-position: center;
   background-size: cover;
-  width: 500px;
-  height: 200px;
+  width: 40%;
+  height: 100%;
   margin: 10px;
   border: 1px solid black;
 `;
 
 const BottomRightImg = styled.div`
-  font-size: 55px;
-  padding: 10px;
+  font-size: 40px;
+  /* padding: 10px; */
   color: #c0c0c0;
   background-color: black;
   /* background-image: url("https://cdn.catholicnews.co.kr/news/photo/202001/22135_43951_5525.jpg"); */
-  background-position: center;
-  background-size: cover;
+  /* background-position: center;
+  background-size: cover; */
   width: 100%;
-  height: 200px;
+  height: 100%;
   margin: 10px;
+  padding: 0px 40px;
   border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
